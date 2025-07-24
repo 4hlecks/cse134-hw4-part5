0249c17c25733c98181e5181f6b529565896ea00
alex <alatienza@ucsd.edu>
2025-07-24 04:10:13 -0700

    couple more error fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-    assetsInclude: ['media/*.svg'], // Adjust path if needed
+    assetsInclude: [
+      'media/*.svg',
+      'media/*.png',
+      'media/*.jpg',
+      'media/*.jpeg',
+      'media/*.webp'
+    ],
   },
 });
